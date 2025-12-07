9aa86014ca0126938600aa77417545536b36efed
ktsoupos <konstantinostsoupos@gmail.com>
2025-12-07 21:03:40 +0200

    implement synchronus accumulator

---
+++ src/calc/accumulator.v
@@ -0,0 +1,16 @@
+module accumulator(input wire clk, 
+                   input wire btnac,
+                   input wire btnc,
+                   input wire [15:0] alu_result_low,
+                   output reg [15:0] acc_low);
+
+    
+    always @(posedge clk) begin
+        if (btnac) begin
+            acc_low <= 16'b0;
+        end 
+        else if (btnc) begin
+            acc_low <= alu_result_low;
+        end
+    end
+endmodule
---
+++ src/calc/calc.v
@@ -0,0 +1,12 @@
+module calc(input wire clk,
+            input wire btnac,
+            input wire btnl,
+            input wire btnr,
+            input wire btnc,
+            input wire btnd,
+            input wire [15:0] sw,
+            output wire [15:0] led);
+
+    
+
+endmodule
